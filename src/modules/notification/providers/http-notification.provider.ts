c32ab003fd0f238530dfa250f344ffaa1cc5bda7
Oleksandr Ivanyshyn <ivanishin.alexander@lll.kpi.ua>
2026-06-27 16:03:36 +0300

    refactor: use httpClient util in HttpNotificationProvider

--- src/modules/notification/providers/http-notification.provider.ts
+++ src/modules/notification/providers/http-notification.provider.ts
@@ -1,6 +1,5 @@
 import type { NotificationPort } from '../../../common/interfaces/notification-port.interface';
-import { AppError } from '../../../common/errors';
-import { HttpStatus } from '../../../common/constants/http-status.constant';
+import { httpClient } from '../../../common/utils/http-client.util';
 
 export class HttpNotificationProvider implements NotificationPort {
   constructor(private readonly notificationUrl: string) {}
@@ -36,17 +35,10 @@ export class HttpNotificationProvider implements NotificationPort {
   }
 
   private async post(path: string, body: unknown): Promise<void> {
-    const response = await fetch(`${this.notificationUrl}${path}`, {
+    await httpClient<void>(`${this.notificationUrl}${path}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body),
     });
-
-    if (!response.ok) {
-      throw new AppError(
-        HttpStatus.INTERNAL_SERVER_ERROR,
-        `Notification service error: ${response.status}`,
-      );
-    }
   }
 }
